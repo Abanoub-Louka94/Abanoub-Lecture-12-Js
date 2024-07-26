553df17064ebf13104ff0ec0a47f9323c45511c1
Abanoub-Louka <abanoub.louka94@gmail.com>
2024-07-27 02:37:24 +0300

    adding css file and editing js file

--- js/main.js
+++ js/main.js
@@ -17,10 +17,36 @@ function Button(background_color, text_color, text) {
 
 };
 
-
 let downloadButton = new Button('#273c75', '#dcdde1', 'submit');
 
 let uploadButton = new Button('#273c75', '#dcdde1', 'submit');
 
 
+// creating container
+const container = document.createElement('div');
 
+container.classList.add('container');
+
+document.body.appendChild(container);
+
+// creating header
+const header = document.createElement('h1');
+
+const headerNode = document.createTextNode('buttons');
+
+header.appendChild(headerNode);
+
+header.classList.add('header');
+
+document.body.appendChild(header);
+
+container.appendChild(header)
+
+// creating buttons container
+const buttonsContainer = document.createElement('div');
+
+buttonsContainer.classList.add('buttons-container');
+
+document.body.appendChild(buttonsContainer);
+
+container.appendChild(buttonContainer);
